ac121c845b7f7d3940b4a8acc65cd007c8528516
bisolutions-git <41757935+bisolutions-git@users.noreply.github.com>
2025-08-17 15:41:00 +0300

    Fix

--- superset_config.py
+++ superset_config.py
@@ -28,7 +28,7 @@ SECRET_KEY = '58bQh4VjsudDEIyICyNhA75+WGm793RD/B1jBRNO/FtATs329XR1CuiL'
 # The SQLAlchemy connection string to your database backend
 # PostgreSQL configuration - Docker container connection
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    'postgresql://superset:Edrak@Superset2025@edrak_db:5432/edrak_analytics'
+    'postgresql://superset:Edrak_Superset2025@edrak_db:5432/edrak_analytics'
 
 # Flask-WTF flag for CSRF
 WTF_CSRF_ENABLED = True
